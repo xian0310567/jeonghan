a084e23552a0224cefe0f0eea9a03c86c3d9bdb7
the_comma2537 <the_comma2537@naver.com>
2023-04-14 01:50:24 +0900

    [server] prisma init

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,19 @@
 import express, { Express, Request, Response } from "express";
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
 
 const app: Express = express();
 const port = 5000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Typescript + Node.js + Express Server");
+app.post("/", async (req: Request, res: Response) => {
+  const createUser = await prisma.user.create({
+    data: {
+      email: "as1df@asdf.com",
+      name: "이정한1"
+    }
+  })
+  res.send({createUser});
 });
 
 app.listen(port, () => {
