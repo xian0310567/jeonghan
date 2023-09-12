ebd700e95fe6d262d01437027cd919ef294658f8
xian0310567 <the_comma2537@naver.com>
2023-09-12 18:28:52 +0900

    Feat: 발행 기능 완료

--- src/hooks/editPosts/useSubmitPost/service/submitPostService.ts
+++ src/hooks/editPosts/useSubmitPost/service/submitPostService.ts
@@ -1,19 +1,27 @@
 import axios from "axios";
+import { useRouter } from "next/router";
+import { message } from "antd";
 import { useS3Upload } from "next-s3-upload";
 
 import { SubmitPostStateCallback } from "../lib/useSubmitPostState";
 
 const submitPostService = (state: SubmitPostStateCallback) => {
+  const router = useRouter();
   const { uploadToS3 } = useS3Upload();
 
   const submitPost = async () => {
     const { url } = await uploadToS3(state.thumbnailImage.get() as File);
 
-    axios.post("/api/posts", {
-      title: state.title.get(),
-      contents: state.contents.get(),
-      thumbnailImage: url,
-    });
+    axios
+      .post("/api/posts", {
+        title: state.title.get(),
+        contents: state.contents.get(),
+        thumbnailImage: url,
+      })
+      .then(() => {
+        message.success("발행되었습니다");
+        router.push("/");
+      });
   };
 
   return { submitPost };
