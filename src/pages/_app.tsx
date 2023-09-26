94b5a6f172efa728f91ade0564851f7b5c524d7c
JeongHan Lee <the_comma2537@naver.com>
2023-09-27 05:09:19 +0900

    Fix: 파비콘 삭제

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,6 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>jeonghan_log :: 좋아하는것을 나답게</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <Layout>
         {/* <Container> */}
