fda85f58f0a72cd7ad81264af8c26839f35a70df
JeongHan Lee <the_comma2537@naver.com>
2023-09-29 21:06:07 +0900

    Feat: editPosts Container 추가

--- src/pages/editPosts.tsx
+++ src/pages/editPosts.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import dynamic from "next/dynamic";
 
+import Container from "@/components/atoms/Container";
 import PostTitleEditor from "@/components/feature/editPosts/PostTitleEditor";
 import EditTags from "@/components/feature/editPosts/EditTags";
 import SubmitPost from "@/components/feature/editPosts/SubmitPost";
@@ -16,12 +17,12 @@ const EditPosts = () => {
   const { title, contents, thumbnailImage, usePublishPost } = useSubmitPost();
 
   return (
-    <div>
+    <Container>
       <PostTitleEditor title={title} thumbnailImage={thumbnailImage} />
       <EditTags />
       <DynamicEditor contents={contents} />
       <SubmitPost usePublishPost={usePublishPost} />
-    </div>
+    </Container>
   );
 };
 
