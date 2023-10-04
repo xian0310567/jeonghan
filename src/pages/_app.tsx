dc47aa6d7fd58ee6736bc6afb128dfe3c265e4df
JeongHan Lee <the_comma2537@naver.com>
2023-10-05 03:43:37 +0900

    Feat: 구글 검색 소유권 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,10 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>jeonghan_log :: 좋아하는것을 나답게</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="google-site-verification"
+          content="_T5a5Sm87KoXsHjZP6kmrlYHGHhJrVwo35hL2-ShV54"
+        />
       </Head>
       <Layout>
         <Component {...pageProps} />
